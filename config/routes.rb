89faea896464bbeb24f07dd14c448c3b7a613eb1
a.ishihara <a.ishihara@lexues.co.jp>
2019-02-05 11:19:51 +0900

    ホーム画面追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
 
   # 特定のコントローラ内アクションにマッピングさせるようルーターに要求している
   resources :ideas
-  # rootメソッドを使用することで、Railsがルート'/'とすべき場所を指定できる
-  root to: redirect('/ideas')
 
   get 'about', to: 'pages#about'
+
+  get 'home', to: 'pages#home'
+  # rootメソッドを使用することで、Railsがルート'/'とすべき場所を指定できる
+  root to: redirect('/home')
 end
