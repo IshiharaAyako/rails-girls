63f9dc8de4920267012e6b7a43d1e1df9f0e2c68
a.ishihara <a.ishihara@lexues.co.jp>
2019-02-06 17:18:32 +0900

    ルート修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
+  get '/', to: 'pages#home'
+
+  get 'about', to: 'pages#about'
+
   get 'profile', to: 'pages#info'
 
   # 特定のコントローラ内アクションにマッピングさせるようルーターに要求している
   resources :ideas
-
-  get 'about', to: 'pages#about'
-
-  get 'home', to: 'pages#home'
-  # rootメソッドを使用することで、Railsがルート'/'とすべき場所を指定できる
-  root to: redirect('/home')
 end
